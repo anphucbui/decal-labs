0470928b3d4b98a365b0f413660e87b6340a84ac
An Bui <anphucbui@berkeley.edu>
2019-10-01 18:33:47 -0700

    done lab b3

--- b3/phonebook_skeleton.py
+++ b3/phonebook_skeleton.py
@@ -12,6 +12,10 @@ def main():
 
     elif sys.argv[1] == "new":
         # YOUR CODE HERE #
+        name = " ".join(sys.argv[2:])
+
+        with open(PHONEBOOK_ENTRIES, 'a') as f:
+            f.write(name + "\n")
 
     elif sys.argv[1] == "list":
         if not os.path.isfile(PHONEBOOK_ENTRIES) or os.path.getsize(
@@ -19,19 +23,30 @@ def main():
             print("phonebook is empty")
         else:
             # YOUR CODE HERE #
+            with open(PHONEBOOK_ENTRIES, 'r') as f:
+                for line in f:
+                    print(line)
 
     elif sys.argv[1] == "remove":
         name = " ".join(sys.argv[2:])
         # YOUR CODE HERE #
+        with open(PHONEBOOK_ENTRIES, 'r') as f:
+            lines = f.readlines()
+        with open(PHONEBOOK_ENTRIES, 'w') as f:
+            for line in lines:
+                if name not in line:
+                    f.write(line)
 
     elif sys.argv[1] == "clear":
         # YOUR CODE HERE #
+        open(PHONEBOOK_ENTRIES, 'w').close()
 
     else:
         name = " ".join(sys.argv[1:])
         with open(PHONEBOOK_ENTRIES, 'r') as f:
             lookup = "".join(filter(lambda line: name in line, f.readlines()))
             # YOUR CODE HERE #
+            print(lookup)
 
 
 if __name__ == "__main__":
--- b3/phonebook_skeleton.sh
+++ b3/phonebook_skeleton.sh
@@ -8,20 +8,25 @@ if [ "$#" -lt 1 ]; then
 
 elif [ "$1" = "new" ]; then
     # YOUR CODE HERE #
+    echo "$2 $3 $4" >> $PHONEBOOK_ENTRIES
 
 elif [ "$1" = "list" ]; then
     if [ ! -e $PHONEBOOK_ENTRIES ] || [ ! -s $PHONEBOOK_ENTRIES ]; then
         echo "phonebook is empty"
     else
         # YOUR CODE HERE #
+        cat $PHONEBOOK_ENTRIES
     fi
 
 elif [ "$1" = "remove" ]; then
     # YOUR CODE HERE #
+    sed -i "" "/$2 $3/d" $PHONEBOOK_ENTRIES
 
 elif [ "$1" = "clear" ]; then
     # YOUR CODE HERE #
+    > $PHONEBOOK_ENTRIES
 
 else
      # YOUR CODE HERE #
+     grep "$1 $2" $PHONEBOOK_ENTRIES
 fi
